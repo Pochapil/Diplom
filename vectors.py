dd72585f68fab928423da899490c79c81a6c72e3
Pochapil <bagrat37@mail.ru>
2023-03-14 19:35:31 +0300

    начал ругаться на arctan(0)

--- vectors.py
+++ vectors.py
@@ -13,6 +13,11 @@ def get_angles_from_vector(vector):
             phi = np.arctan(y / x)
         else:
             phi = np.arctan(y / x) + 2 * np.pi
+    elif x == 0:
+        if y > 0:
+            phi = np.pi / 2
+        else:
+            phi = 3 * np.pi / 2
     else:
         phi = np.arctan(y / x) + np.pi
     return phi, theta
@@ -22,13 +27,18 @@ def get_angles_from_vector_one_dimension(vector):
     x = vector[0]
     y = vector[1]
     z = vector[2]
-    r = x**2 + y**2 + z**2
-    theta = np.arccos(z/r)  # np.arccos(z/r)
+    r = x ** 2 + y ** 2 + z ** 2
+    theta = np.arccos(z / r)  # np.arccos(z/r)
     if x > 0:
         if y >= 0:
             phi = np.arctan(y / x)
         else:
             phi = np.arctan(y / x) + 2 * np.pi
+    elif x == 0:
+        if y > 0:
+            phi = np.pi / 2
+        else:
+            phi = 3 * np.pi / 2
     else:
         phi = np.arctan(y / x) + np.pi
     return phi, theta
