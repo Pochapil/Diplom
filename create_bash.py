342284bc4d74b8fef6b3745038d3cbabba624f38
Pochapil <bagrat37@mail.ru>
2023-08-22 23:06:40 +0300

    mian -> main

--- create_bash.py
+++ create_bash.py
@@ -3,8 +3,8 @@ N_cores = 4
 i_angle = [60]
 betta_mu = [40]
 
-mc2 = [30]
-a_portion = [0.65]
+mc2 = [10, 14, 20, 27, 38, 50, 74, 100, 143, 200]
+a_portion = [0.25, 0.65]
 
 fi_0 = [20 * i for i in range(3)]
 
@@ -22,7 +22,7 @@ for i_angle_index in range(len(i_angle)):
         for mc2_index in range(len(mc2)):
             for a_portion_index in range(len(a_portion)):
                 for fi_0_index in range(len(fi_0)):
-                    f.write('python3 mian_with_args.py ')
+                    f.write('python3 main_with_args.py ')
                     f.write('%d %d %d %0.2f %d &\n' % (
                     i_angle[i_angle_index], betta_mu[betta_mu_index], mc2[mc2_index], a_portion[a_portion_index],
                     fi_0[fi_0_index]))
