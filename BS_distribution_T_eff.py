1042fabaa26bac71b4c394593eeaaddbe4a213df
Pochapil <bagrat37@mail.ru>
2023-10-16 12:11:34 +0300

    комментарии

--- BS_distribution_T_eff.py
+++ BS_distribution_T_eff.py
@@ -6,21 +6,29 @@ import config  # const
 
 
 def get_Teff_distribution(R_e, delta_ns, A_normal):
-    # решение зависит от n размера пространства !!! взял n=3 везде
+    # решение зависит от n размера пространства !!! взял n=3 везде;
+
+    # над 10 формулой:
+    # The case n = 2 corresponds to the spherically diverging accretion column;
+    # the case n = 3 is a good approximation to the flow pattern near the magnetic poles
+    # when matter falls along the field lines of a magnetic dipole.
+
     l0 = A_normal / (2 * delta_ns)  # длина аккреции на поверхности, м. взял как в статье
     u0 = 3 * config.H ** 2 / 8 / np.pi  # значение плотности излучения на поверхности
     d0 = delta_ns  # ширина аккреции на поверхности м
+    # the amount of matter s falling on to a unit surface area of the neutron star per unit time as a fixed quantity:
     s = config.M_accretion_rate / A_normal
 
     # 50 формула статья
     gamma = (config.c * config.R_ns * A_normal * 3) / \
             (config.k * delta_ns ** 2 * config.M_accretion_rate * 2 * config.ksi_rad)
-    #print("gamma = %f" % gamma)
+    # print("gamma = %f" % gamma)
 
     # 51 формула статья
     eta = ((8 * config.k * u0 * delta_ns ** 2 * 2 * config.ksi_rad) /
            (21 * config.c * (2 * config.G * config.M_ns * config.R_ns) ** (1 / 2) * 3)) ** (1 / 4)
-    #print("eta = %f" % eta)
+
+    # print("eta = %f" % eta)
 
     # 30 формула, du/dksi; dv/dksi = производная от 3 равенства
     # возвращает u, v
@@ -46,7 +54,7 @@ def get_Teff_distribution(R_e, delta_ns, A_normal):
             return x - f(x) / df(x)
 
         delta = 0.001  # точность для метода ньютона
-        ksi1 = 14.3
+        ksi1 = 14.3  # начальное предположение
         ksi2 = nuton(ksi1)
         while np.abs((ksi1 - ksi2)) > delta:
             ksi1 = ksi2
@@ -128,7 +136,7 @@ def get_Teff_distribution(R_e, delta_ns, A_normal):
     # формула 37, 1 - полная светимость
     L_x = (1 - beta) * config.M_accretion_rate * config.G * config.M_ns / config.R_ns
 
-    #print("bettaBS = %f" % betta)
-    #print("e = %.5f" % e)
+    # print("bettaBS = %f" % betta)
+    # print("e = %.5f" % e)
 
     return Teff, ksiShock, L_x, beta
