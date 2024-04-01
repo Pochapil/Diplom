b1522aed2d1767d89189b28c9a95e81a68c88a3a
Pochapil <bagrat37@mail.ru>
2024-04-01 11:55:50 +0300

    функцию для папки изменил - все в new_data

--- linuxoid/config.py
+++ linuxoid/config.py
@@ -8,14 +8,14 @@ fi_0 = 20
 
 
 file_folder = 'figs/loop/'
-file_folder_angle = 'i=%d betta_mu=%d/' % (i_angle, betta_mu)
-file_folder_args = 'mc2=%d/a=%0.2f fi_0=%d/' % (mc2, a_portion, fi_0)
-full_file_folder = file_folder + file_folder_angle + file_folder_args
+file_folder_angle_args = 'i=%d betta_mu=%d/' % (i_angle, betta_mu)
+file_folder_accretion_args = 'mc2=%d/a=%0.2f fi_0=%d/' % (mc2, a_portion, fi_0)
+full_file_folder = file_folder + file_folder_angle_args + file_folder_accretion_args
 
 #print(full_file_folder)
 
 def update():
-    global file_folder,file_folder_angle,file_folder_args, full_file_folder
+    global file_folder,file_folder_angle_args,file_folder_accretion_args, full_file_folder
     file_folder = 'figs/loop/'
     file_folder_angle = 'i=%d betta_mu=%d/' % (i_angle, betta_mu)
     file_folder_args = 'mc2=%d/a=%0.2f fi_0=%d/' % (mc2, a_portion, fi_0)
